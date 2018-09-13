e6b541773c92423d713d9a8af8b449095e09d97a
Jentry Jibben and richard laws <richard@>
2018-09-12 20:38:22 -0700

    add addition calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-alert("HI?");
+//biz logic
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+//UI logic
+$(document).ready(function() {
+	$("#calc form").submit(function(event){
+
+		var number1 = parseInt($("input#number1").val());
+		var number2 = parseInt($("input#number2").val());
+
+		// parseInt(number1).val();
+		// parseInt(number2).val();
+			// var number1 = parseInt("input#number1").val();
+			// var number2 = parseInt("input#number2").val();
+			//console.log(number1)
+
+			var result = add(number1,number2);
+			console.log(result);
+			event.preventDefault();
+
+	});
+
+
+
+});
+
+// $("form.calc").submit(function(event){
+// 		var add = function(number1, number2) {
+// 			return number1 + number2;
+//
+// 		var number1 = parseInt("input#numb1").val();
+// 		var number2 = parseInt("input#numb2").val();
+// 		console.log(number1)
+//
+// 		alert(add(number1,number2));
+// 		console.log(number2)
+// 		event.preventDefault();
+//
+// 	};
+// });
